c96ce4f8b04c549310ef06c396efd616375e8a94
Juan Portillo <juan@goowee.io>
2017-06-14 18:54:00 -0600

    add api key

---
+++ app/utils/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const key = '3a1a38e349673e2cd1b16e9944aa4962';
+const current = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&type=accurate&APPID=' + key;
+const forecast = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&type=accurate&APPID=' + key + '&cnt=5';
+
+module.exports = {
+
+}
